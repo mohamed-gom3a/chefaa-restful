493fecafbfb73d5a7bd0364f4bd6d79888d49633
ipownz1 <ipownz1@gmail.com>
2025-08-17 17:54:16 +0300

    just cleaned up project from temp modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,16 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AccessJwtAuthGuard } from './auth/access-jwt-auth.guard';
 import { AuthModule } from './auth/auth.module';
-import { ProductModule } from './models/product/product.module';
+import { CategoryModule } from './models/category/category.module';
+import { MedicationModule } from './models/medication/medication.module';
 import { UserModule } from './models/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-
-import { CategoryModule } from './category/category.module';
-import { MedicationModule } from './medication/medication.module';
-import { PurchaseModule } from './models/purchase/purchase.module';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
     PrismaModule,
-    ProductModule,
-    PurchaseModule,
     MedicationModule,
     CategoryModule,
   ],
